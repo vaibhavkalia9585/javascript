3365383206b751ae19ac8f708f95333efdd88869
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-09-10 20:09:30 +0000

    let, const & var

---
+++ .github/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553 //const means it can not be changed
+let accountEmail = "vaibhavkalia9585@gmail.com"
+var accountPassword = "12345"
+accountCity = "Toronto"
+
+
+//accountId = 2 //not allowed 
+accountEmail = "ivaibhavkalia@gmail.com"
+accountPassword = "1234567"
+accountCity = "Phagwara"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
