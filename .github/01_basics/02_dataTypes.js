1393a7aa54987646679369b14835df7c6d8a47b4
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-09-10 20:29:15 +0000

    datatypes

---
+++ .github/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodeJS , not browser
+
+let name = "vaibhav"
+let age = 19;
+let isLoggedIn = false;
+let state = null;
+let undefined;
+
+//number = 2 to power 53
+//bigint for very big value
+//string = ""
+//boolean = true/false
+//null is a standalone value (IT IS AN OBJECT)
+//undefined = value is not defined yet
+//symbol unique use only. 
+//object
+
+console.log(typeof age);
+console.log(typeof undefined)
