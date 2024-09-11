7ca3ce125e11cb2eea9dac463665f314cbe32679
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-09-11 15:46:57 +0000

    conversions learned

---
+++ .github/01_basics/03_conversionOps.js
@@ -0,0 +1,11 @@
+//let score = "33" //33
+//let score = "33abc" //Nan
+//let score = null //0
+//let score = undefined//Nan
+let score = true;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
